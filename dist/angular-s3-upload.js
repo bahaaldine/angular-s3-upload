2a7188dd8a681b38e62fb94276d33889005e42c3
Bahaaldine Azarmi <bahaaldine@gmail.com>
2014-10-02 16:27:54 +0200

    changed module name

--- dist/angular-s3-upload.js
+++ dist/angular-s3-upload.js
@@ -1,4 +1,4 @@
-/*! angular-s3-upload - v0.0.5 - 2014-10-02
+/*! angular-s3-upload - v0.0.8 - 2014-10-02
 * Copyright (c) 2014 ; Licensed  */
   /*! angular-facebook-insight - v0.6.1 - 2014-07-13
 * Copyright (c) 2014 ; Licensed  */
