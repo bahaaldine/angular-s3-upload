d1514fc1c8f116fd650ad8dfa9963ceb5e2d31b8
Bahaaldine Azarmi <bahaaldine@gmail.com>
2014-11-20 10:40:15 +0100

    cleaning jshint warnings

--- dist/angular-s3-upload.js
+++ dist/angular-s3-upload.js
@@ -1,16 +1,14 @@
-/*! angular-s3-upload - v0.0.20 - 2014-11-19
+/*! angular-s3-upload - v0.0.24 - 2014-11-20
 * Copyright (c) 2014 ; Licensed  */
   /*! angular-facebook-insight - v0.6.1 - 2014-07-13
 * Copyright (c) 2014 ; Licensed  */
 'use strict';
 
-var page_id = 0;
-
 angular.module("angular-s3-upload-tpls", ["templates/angular-s3-upload-button.html"]);
 'use strict';
 
 var ngS3Upload = angular.module('angular-s3-upload', ["angular-s3-upload-tpls"]);
-ngS3Upload.directive('ngS3Upload', [ '$upload', function($upload) {
+ngS3Upload.directive('ngS3Upload', [ '$upload', '$q', function($upload, $q) {
   return {
     restrict: 'E',
     replace: true,
@@ -26,7 +24,9 @@ ngS3Upload.directive('ngS3Upload', [ '$upload', function($upload) {
       progressCallback: '=',
       awsApi:'=',
       bucket: '@',
-      filename: '@'
+      filename: '@',
+      maxWidth: '@',
+      maxHeight: '@'
     },
     templateUrl: 'templates/angular-s3-upload-button.html',
     link: function(scope, element, attr) {
@@ -49,43 +49,123 @@ ngS3Upload.directive('ngS3Upload', [ '$upload', function($upload) {
           });
       };
 
+      var dataURItoBlob = function(dataURI) {
+          var byteString;
+          if (dataURI.split(',')[0].indexOf('base64') >= 0)
+              byteString = atob(dataURI.split(',')[1]);
+          else
+              byteString = unescape(dataURI.split(',')[1]);
+
+          var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+          var ia = new Uint8Array(byteString.length);
+          for (var i = 0; i < byteString.length; i++) {
+              ia[i] = byteString.charCodeAt(i);
+          }
+          return {blob: new Blob([ia], {type:mimeString}), type: mimeString};
+      };
+
+      var resizeImage = function(file) {
+        var deferred = $q.defer();  
+        var canvas = document.createElement('canvas');
+       
+        var img = new Image();
+        img.src = URL.createObjectURL(file);
+        img.onload = function() {
+          var ctx = canvas.getContext("2d");
+          ctx.drawImage(img, 0, 0);
+
+          var width = img.width;
+          var height = img.height;
+          if (width > height) {
+            if (width > $scope.maxWidth) {
+              height *= $scope.maxWidth / width;
+              width = $scope.maxWidth;
+            }
+          } else {
+            if (height > $scope.maxHeight) {
+              width *= $scope.maxHeight / height;
+              height = $scope.maxHeight;
+            }
+          }
+          canvas.width = width;
+          canvas.height = height;
+
+          ctx = canvas.getContext("2d");
+          ctx.drawImage(img, 0,0, width, height);
+
+          var dataURL = canvas.toDataURL('image/png', 1.0);
+          deferred.resolve(dataURItoBlob(dataURL));
+        };
+
+        return deferred.promise;
+      };
+
       $scope.onFileSelect = function($files) {
         for ( var i = 0; i < $files.length; i++) {
           var file = $files[i];
-          if ( typeof $scope.filename == "undefined" ) {
-            $scope.filename = $scope.createUUID();
+          if ( angular.isDefined($scope.filename) ) {
+              $scope.filename = $scope.createUUID();
           }
           var fullPath = $scope.key+"/"+$scope.filename;
-          if ( typeof $scope.path != "undefined" ) {
+          if ( !angular.isDefined($scope.path) ) {
             fullPath = $scope.key+"/"+$scope.path+"/"+$scope.filename;
           }
-          $scope.upload = $upload.upload({
-            url: 'https://'+$scope.bucket+'.s3.amazonaws.com/',
-            method: 'POST',
-            data: {
-                'key' : fullPath,
-                'acl' : 'public-read',
-                'Content-Type' : file.type,
-                'AWSAccessKeyId': $scope.aws.key,
-                'Policy' : $scope.aws.policy,
-                'Signature' : $scope.aws.signature
-            },
-            file: file
-          }).progress(function(evt) {
-              // parseInt(100.0 * evt.loaded / evt.total));
-              if ( typeof $scope.progressCallback.progress !== "undefined" ) {
-                $scope.progressCallback(evt);
-              }
-          }).success(function(data, status, headers, config) {
-            if ( typeof $scope.successCallback !== "undefined" ) {
-              $scope.successCallback($scope.filename, $scope.index, data, status, headers, config);
-            }
-          }).error(function(data, status, headers, config) {
-            if ( typeof $scope.failureCallback !== "undefined" ) {
-              $scope.failureCallback($scope.index, data, status, headers, config);
-            } // die silently
-          });
+
+          var config = {};
+          if ( !angular.isDefined($scope.maxWidth) || !angular.isDefined($scope.maxHeight) ) {
+            config = {
+              transformRequest: angular.identity,
+              url: 'https://'+$scope.bucket+'.s3.amazonaws.com/',
+              method: 'POST',
+              data: {
+                  'key' : fullPath,
+                  'acl' : 'public-read',
+                  'Content-Type' : file.type,
+                  'AWSAccessKeyId': $scope.aws.key,
+                  'Policy' : $scope.aws.policy,
+                  'Signature' : $scope.aws.signature
+              },
+              file: file
+            };
+            uploadFile(config);
+          } else {
+            resizeImage(file).then(function(resized) {
+              config = {
+                transformRequest: angular.identity,
+                url: 'https://'+$scope.bucket+'.s3.amazonaws.com/',
+                method: 'POST',
+                data: {
+                    'key' : fullPath,
+                    'acl' : 'public-read',
+                    'Content-Type' : resized.type,
+                    'AWSAccessKeyId': $scope.aws.key,
+                    'Policy' : $scope.aws.policy,
+                    'Signature' : $scope.aws.signature,
+                    'file': resized.blob
+                }
+              };
+              uploadFile(config);
+            });
+          }
         }
+      };
+
+      var uploadFile = function(config) {
+        $scope.upload = $upload.upload(config).progress(function(evt) {
+            // parseInt(100.0 * evt.loaded / evt.total));
+            if ( typeof $scope.progressCallback.progress !== "undefined" ) {
+              $scope.progressCallback(evt);
+            }
+        }).success(function(data, status, headers, config) {
+          if ( typeof $scope.successCallback !== "undefined" ) {
+            $scope.successCallback($scope.filename+ '?cb=' + (new Date().getTime()).toString(), $scope.index, data, status, headers, config);
+          }
+        }).error(function(data, status, headers, config) {
+          if ( typeof $scope.failureCallback !== "undefined" ) {
+            $scope.failureCallback($scope.index, data, status, headers, config);
+          } // die silently
+        });
       };
     }
   };
